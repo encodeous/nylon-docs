85fef65494c6b54fd7008b32037ba1a56140172d
encodeous <encodeous7@gmail.com>
2026-04-28 10:52:14 -0400

    doc-site: init

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		mermaid({
+			autoTheme: true
+		}),
+		starlight({
+			title: 'nylon docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/encodeous/nylon' }],
+			sidebar: [
+				{
+					label: 'Overview',
+					link: '/',
+				},
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			logo: {
+				light: './src/content/docs/assets/logo_light.svg',
+				dark: './src/content/docs/assets/logo_dark.svg'
+			}
+		}),
+	],
+	vite: {
+		server: {
+			watch: {
+				followSymlinks: true,
+			},
+		},
+		resolve: {
+			preserveSymlinks: true
+		}
+	}
+});
