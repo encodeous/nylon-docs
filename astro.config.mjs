e5ece67ef07f765996721b0d0d945a8be6b23b02
encodeous <encodeous7@gmail.com>
2026-04-30 00:40:46 -0400

    doc: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,55 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import mermaid from 'astro-mermaid';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import mermaid from "astro-mermaid";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		mermaid({
-			autoTheme: true
-		}),
-		starlight({
-			title: 'nylon docs',
-			routeMiddleware: './src/routeData.ts',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/encodeous/nylon' }],
-			sidebar: [
-				{
-					label: 'Overview',
-					link: '/',
-				},
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			logo: {
-				light: './src/content/docs/assets/logo_light.svg',
-				dark: './src/content/docs/assets/logo_dark.svg'
-			}
-		}),
-	],
-	vite: {
-		server: {
-			watch: {
-				followSymlinks: true,
-			},
-		},
-		resolve: {
-			preserveSymlinks: true
-		}
-	}
+  site: "https://nylon.jq.ax",
+  integrations: [
+    mermaid({
+      autoTheme: true,
+    }),
+    starlight({
+      title: "nylon docs",
+      routeMiddleware: "./src/routeData.ts",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/encodeous/nylon",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Overview",
+          link: "/",
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      logo: {
+        light: "./src/content/docs/assets/logo_light.svg",
+        dark: "./src/content/docs/assets/logo_dark.svg",
+      },
+    }),
+    sitemap(),
+  ],
+  vite: {
+    server: {
+      watch: {
+        followSymlinks: true,
+      },
+    },
+    resolve: {
+      preserveSymlinks: true,
+    },
+  },
 });
--- src/routeData.ts
+++ src/routeData.ts
@@ -1,13 +1,16 @@
-import { defineRouteMiddleware } from '@astrojs/starlight/route-data';
+import { defineRouteMiddleware } from "@astrojs/starlight/route-data";
 
 export const onRequest = defineRouteMiddleware((context) => {
+  if (!context.locals.starlightRoute || !context.locals.starlightRoute.entry) {
+    return;
+  }
   const { entry } = context.locals.starlightRoute;
 
   // The default path would be 'src/content/docs/page-slug.md'
-  const repoPath = 'https://github.com/encodeous/nylon/edit/main/docs';
+  const repoPath = "https://github.com/encodeous/nylon/edit/main/docs/";
 
   context.locals.starlightRoute.editUrl = new URL(
-    `${entry.filePath.replace('src/content/', '')}`, 
-    repoPath
+    `${entry.filePath.replace("src/content/docs/", "")}`,
+    repoPath,
   );
 });
